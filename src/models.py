92548e2ab6e4cf0c86dc02081c8c8e894f872e3a
edianibarrola <edianibarrola@gmail.com>
2021-01-22 16:10:21 +0000

    update

--- src/models.py
+++ src/models.py
@@ -2,18 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    full_name = db.Column(db.String(120), unique=False, nullable=False)
+    address = db.Column(db.String(200), unique=False, nullable=True)
+    phone = db.Column(db.Integer, unique=False, nullable=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Contact %r>' % self.full_name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "full_name": self.full_name,
+            "address": self.address,
+            "phone": self.phone,
+            "email": self.email
+
             # do not serialize the password, its a security breach
         }
