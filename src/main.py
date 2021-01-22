c6671cae55600f23ed15a0e5e33fe82c6fa55d97
edianibarrola <edianibarrola@gmail.com>
2021-01-22 17:39:29 +0000

    update

--- src/main.py
+++ src/main.py
@@ -35,10 +35,33 @@ def sitemap():
 def get_all():
     
     response_body = Contact.query.all()
+    response_body = list(map(lambda x: x.serialize(), response_body))
     
 
     return jsonify(response_body), 200
 
+@app.route('/contacts', methods=['POST'])
+def add_contact():
+    contact_info= request.get_json()
+    new_contact= Contact(full_name=contact_info['full_name'], address= contact_info['address'], phone=contact_info['phone'], email=contact_info['email'], )
+    db.session.add(new_contact)
+    db.session.commit()
+    response = Contact.query.all()
+    response = list(map(lambda x: x.serialize(), response))
+
+    return jsonify(response), 200
+
+@app.route('/contacts/<int:id>', methods=['DELETE'])
+def delete_contact(id):
+    contact_to_delete = Contact.query.get(id)
+    if contact_to_delete is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(contact_to_delete)
+    db.session.commit()
+    response = Contact.query.all()
+    response = list(map(lambda x: x.serialize(), response))
+
+    return jsonify(response), 200
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -6,8 +6,9 @@ class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(120), unique=False, nullable=False)
     address = db.Column(db.String(200), unique=False, nullable=True)
-    phone = db.Column(db.Integer, unique=False, nullable=True)
-
+    phone = db.Column(db.String(20), unique=False, nullable=True)
+    email = db.Column(db.String(200), unique=False, nullable=True)
+    
     def __repr__(self):
         return '<Contact %r>' % self.full_name
 
